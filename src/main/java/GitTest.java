5e2df1a22f19321bc5ece20a7dbcd76d23f26783
18937989694 <1456754774@qq.com>
2018-09-15 18:47:52 +0800

    commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,26 @@
+/**
+ * author:18937
+ * createDate:2018/9/15
+ * time:18:01
+ * AIM：
+ */
+public class GitTest {
+    public static void main(String[] args) {
+
+        System.out.println("Git Test 类");
+        System.out.println("Git Test 类");
+        System.out.println("Git Test 类");   System.out.println("Git Test 类");
+        System.out.println("Git Test 类");
+        System.out.println("Git Test 类");   System.out.println("Git Test 类");
+        System.out.println("Git Test 类");
+        System.out.println("Git Test 类");   System.out.println("Git Test 类");
+        System.out.println("Git Test 类");
+        System.out.println("Git Test 类");   System.out.println("Git Test 类");
+        System.out.println("Git Test 类");
+        System.out.println("Git Test 类");   System.out.println("Git Test 类");
+        System.out.println("Git Test 类");
+        System.out.println("Git Test 类");   System.out.println("Git Test 类");
+        System.out.println("Git Test 类");
+        System.out.println("Git Test 类");
+    }
+}
